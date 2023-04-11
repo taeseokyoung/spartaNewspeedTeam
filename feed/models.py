d30d95e0f621767034559c56a63b0b0ea8b1e6cc
Giho Bang <googong522@naver.com>
2023-04-11 15:14:09 +0900

    models.py added post_image

--- feed/models.py
+++ feed/models.py
@@ -5,11 +5,11 @@ class Post(models.Model):
     class Meta:
         db_table ="my_post"
    
-    post_title = models.TextField(default = "write anything") #게시글 제목
-    post_content = models.TextField(default = "write anything") #게시글 내용
+    post_title = models.TextField(default = "write title") #게시글 제목
+    post_content = models.TextField(default = "write content") #게시글 내용
+    post_image = models.TextField(default = "image url")
     created_at = models.DateTimeField(auto_now_add=True) # 생성시간
     updated_at = models.DateTimeField(auto_now=True) # 수정시간
+    
     # user_nickname = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     # user_id = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-
-    
