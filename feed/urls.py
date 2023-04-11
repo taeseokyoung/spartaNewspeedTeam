4ac2b6f2ab2f8804c467ee939dac3f346af78ac8
Tomatopizza <50008271+Tomatopizza@users.noreply.github.com>
2023-04-11 16:25:43 +0900

    Delete feed directory

--- feed/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('posting/', views.posting, name='posting'),
-    #path('posting/modify/', views.posting_modify, name='posting_modifiy'),
-]
