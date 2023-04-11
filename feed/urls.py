3574abb8a2af1e73b0067e62ba68d5c15081b40e
taeseokyoung <xotjrud@gmail.com>
2023-04-11 19:51:06 +0900

    feed views.py / urls.py -ing

--- feed/urls.py
+++ feed/urls.py
@@ -4,6 +4,12 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posting/', views.posting, name='posting'),
-    #path('posting/modify/', views.posting_modify, name='posting_modifiy'),
+
+    path('feed/', views.feed, name='feed'),  # 전체 피드
+    path('feed/my_feed/<int:user>', views.user_feed,
+         name='user_feed'),  # 마이페이지 피드
+    path('feed/upload', views.upload_feed, name='upload_feed'),  # 게시글 삭제
+    path('feed/my_feed/', views.upload_feed, name='upload_feed'),  # 게시글 삭제
+    # 게시글 수정
+    # path('posting/modify/', views.posting_modify, name='posting_modifiy'),
 ]
--- feed/views.py
+++ feed/views.py
@@ -1,50 +1,51 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Post
 
-# Create your views here.
-
-
-# 피드 홈에서는 로그인한 사용자들의 피드가 최신순으로 조회되어야 한다.
+# 로그인한 사용자들의 피드가 최신순으로 조회되어야 한다.
 # 모든 비로그인 방문자들도 메인 피드를 구경할 수 있어야 한다.
 # def home(request):
 #     return redirect('/feed')
+
 
 # 메인 피드에 전체 게시글을 보여준다. : GET
 def feed(request):
     if request.method == 'GET':
         all_feed = Post.objects.all().order_by('-created_at')
-        return render(request, 'tweet/feed.html', {'tweet':all_feed})
-
-
-# 로그인 사용자들이 피드 업로드 시 저장해주는 함수. : POST
-@login_required
-def upload_feed(request):
-    if request.method == 'POST':
-        # uset = request.user
-       my_post = Post()
-        # my_post.author = user
-
-        # request.POST.get('html의 각각의 태그 name이 여기에 적힙니다','')
-       my_post.post_title = request.POST.get('','')
-       my_post.post_content = request.POST.get('','')
-       my_post.post_image = request.POST.get('','')
-       my_post.save()
-       return redirect('/feed')
+        return render(request, 'feed/feed.html', {'tweet': all_feed})
 
 
 # 마이페이지(로그인한 사용자들) 에 자신이 작성한 피드를 불러올 함수. : GET
- 
 @login_required
 def user_feed(request):
     if request.method == 'GET':
         # 사용자가 로그인 되었는지(인증된 사용자가 있는지)
         # user = request.user.is_authenticated
         if user:
-            my_post = Post.objects.
+            my_post = Post.objects.get(user=user).order_by('-created.at')
+            return render(request, 'feed/my_feed.html', {'feed': my_post})
+            # 연결할 때 3강 4:25 부분 참고하기
 
 
-# @login_required
-# def delete_feed():
+# 로그인 사용자들이 피드 업로드 시 저장해주는 함수. : POST
+@login_required
+def upload_feed(request):
+    if request.method == 'POST':
+        # user = request.user
+        my_post = Post()
+        # my_post.author = user
+
+        # request.POST.get('html의 각각의 태그 name이 여기에 적힙니다','')
+        my_post.post_title = request.POST.get('', '')
+        my_post.post_content = request.POST.get('', '')
+        my_post.post_image = request.POST.get('', '')
+        my_post.save()
+        return redirect('/feed')
 
 
+# 로그인한 사용자들이 자신의 피드를 삭제할 함수.
+@login_required
+def delete_feed(request, id):
+    my_feed = Post.objects.get(id=id)
+    my_feed.delete()
+    return redirect('/feed')
